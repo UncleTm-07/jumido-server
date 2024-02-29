c8ae22ac13e2e1092aa79be90668edd688859f3d
Musikhin Vlad <fletspleyhik@gmail.com>
2024-02-29 21:40:57 +0200

    init server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors")
+const http = require("http");
+const mongoose =  require("mongoose");
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("-----> Mongodb connected <-----");
+    server.listen(port, () => {
+        console.log(`Server is listening on port ${port}...`);
+    });
+}).catch((err) => {
+    console.log({err});
+    process.exit(1);
+});
